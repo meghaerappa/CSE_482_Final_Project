36bbbd9a7456feb0c5537b084a8c3b3b2fc6b5dc
Megha Erappa <36482514+meghaerappa@users.noreply.github.com>
2024-07-15 17:02:16 -0400

    Update versionTree.cxx

--- versionTree.cxx
+++ versionTree.cxx
@@ -12,7 +12,7 @@ void bubbleSort(int arr[], int n)
         for (j = 0; j < n - i - 1; j++) {
             if (arr[j] > arr[j + 1]) {
                 swap(arr[j], arr[j + 1]);
-                swapped = true;
+                swapped = false;
 
                 //Added another chnage here
             }
